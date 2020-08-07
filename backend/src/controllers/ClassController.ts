3341f5162d285c9c7da20d3d138de4354ceb23f6
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-08-07 10:41:42 -0300

    Class and Connections controllers

--- backend/src/controllers/ClassController.ts
+++ backend/src/controllers/ClassController.ts
@@ -4,6 +4,7 @@ import db from '../database/connection'
 import convertHourToMinutes from '../utils/convertHourToMinuts'
 
 
+
 export default new class ClassesController {
     async create(req:Request,res:Response){
 
@@ -65,5 +66,34 @@ export default new class ClassesController {
                 error:'Unexpeacted error white creating new class'
             })
         }
+    }
+
+    async index(req:Request,res:Response){
+        const filters = req.query;
+
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        if(!filters.week_day ||!filters.subject ||!filters.time){
+            return res.status(400).json({error:"Missing filter to search classes "})
+        }
+        
+        const timeInMinutes = convertHourToMinutes(time);
+        
+        const classes = await db('classes')
+        .whereExists(function() {
+            this.select('classes_schedule.*')
+                .from('classes_schedule')
+                .whereRaw('`classes_schedule`.`class_id`=`classes`.`id`')
+                .whereRaw('`classes_schedule`.`week_day`= ??',[Number(week_day)])
+                .whereRaw('`classes_schedule`.`from`<=??',[timeInMinutes])
+                .whereRaw('`classes_schedule`.`to` > ??',[timeInMinutes])
+        })
+        .where('classes.subject', '=', subject)
+        .join('users', 'classes.user_id','=','users.id')
+        .select(['classes.*','users.*']);
+
+        return res.json(classes)
     }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,13 @@
-import express, { response } from 'express';
+import express from 'express';
 import ClassController from './controllers/ClassController';
+import ConnectionsControler from './controllers/ConnectionsController';
 
 const routes = express.Router();
 
 routes.post('/classes', ClassController.create);
+routes.get('/classes', ClassController.index);
+
+routes.post('/connections', ConnectionsControler.create);
+routes.get('/connections', ConnectionsControler.index);
 
 export default routes;
