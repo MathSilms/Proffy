4ac467818cf4f7d17dfa7a3b38a7a077318c5379
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-08-14 10:26:42 -0300

    update backend

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,13 @@
-import express from 'express'; 
-import ClassController from './controllers/ClassController'; 
-import ConnectionsControler from './controllers/ConnectionsController'; 
+import express from 'express';
+import ClassController from './controllers/ClassController';
+import ConnectionsControler from './controllers/ConnectionsController';
 
-const routes = express.Router(); 
+const routes = express.Router();
 
-routes.post('/classes', ClassController.create); 
-routes.get('/classes', ClassController.index); 
-routes.post('/connections', ConnectionsControler.create); 
-routes.get('/connections', ConnectionsControler.index); 
+routes.post('/classes', ClassController.create);
+routes.get('/classes', ClassController.index);
+
+routes.post('/connections', ConnectionsControler.create);
+routes.get('/connections', ConnectionsControler.index);
 
 export default routes;
